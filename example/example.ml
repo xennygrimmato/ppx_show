2ad232ef34dfa9b1637ace1c5accc3fce67ea218
Jeremie Dimino <jdimino@janestreet.com>
2015-11-27 12:30:38 +0000

    added README

--- example/example.ml
+++ example/example.ml
@@ -1,4 +1,8 @@
 let () = print_endline ([%show] ([1; 2], Some 42))
-
-
 let () = Format.printf "%a@." [%pp] ([1; 2], Some 42)
+
+external show : _ -> string = "%show"
+external pp : Format.formatter -> _ -> unit = "%pp"
+
+let () = print_endline (show ([1; 2], Some 42))
+let () = Format.printf "%a@." pp ([1; 2], Some 42)
