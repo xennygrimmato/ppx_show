104f315312063f7c6cdfa18f0f06ceccdb6d3b9c
Jeremie Dimino <jdimino@janestreet.com>
2015-11-27 12:24:12 +0000

    support rectypes

--- src/ppx_show.ml
+++ src/ppx_show.ml
@@ -1,6 +1,7 @@
 let () = Generators.linkme
 
 let () =
-  Ast_mapper.register "ppx_show" (fun _ ->
+  Ast_mapper.register "ppx_show" (fun args ->
+    if List.mem "-rectypes" args then Clflags.recursive_types := true;
     { Ast_mapper.default_mapper with structure = fun _ s -> Gen_printer.map_impl s })
 
